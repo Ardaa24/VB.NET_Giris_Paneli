95634f592732ec3ace97f17a704a515406bc561d
Arda24 <92623903+Ardaa24@users.noreply.github.com>
2025-10-03 19:57:43 +0300

    GirisPanel.cs

---
+++ Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Giris_Panel
+{
+    public partial class GirisPanel : Form
+    {
+        public GirisPanel()
+        {
+            InitializeComponent();
+        }
+
+
+        bool move;
+        int mouse_x;
+        int mouse_y;
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void GirisPanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            move = true;
+            mouse_x = e.X;
+            mouse_y = e.Y;
+        }
+
+        private void GirisPanel_MouseUp(object sender, MouseEventArgs e)
+        {
+            move = false;
+        }
+
+        private void GirisPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (move) { 
+                this.SetDesktopLocation(MousePosition.X - mouse_x, MousePosition.Y - mouse_y);
+            }
+        }
+    }
+}
