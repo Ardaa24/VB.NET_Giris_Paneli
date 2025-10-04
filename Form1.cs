5866574c24886751d65284566e19c0d4e9307b75
Arda24 <92623903+Ardaa24@users.noreply.github.com>
2025-10-04 17:58:32 +0300

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,11 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Giris_Panel
 {
     public partial class GirisPanel : Form
     {
+
+        SqlConnection conn = new SqlConnection("Data Source=DESKTOP-29MDJI6\\SQLEXPRESS; initial Catalog=GirisPaneli; Integrated Security=True");
         public GirisPanel()
         {
             InitializeComponent();
@@ -44,6 +47,83 @@ namespace Giris_Panel
             if (move) { 
                 this.SetDesktopLocation(MousePosition.X - mouse_x, MousePosition.Y - mouse_y);
             }
+        }
+
+        private void txtUsername_Enter(object sender, EventArgs e)
+        {
+            if (txtUsername.Text == "Username")
+            {
+                txtUsername.Text = "";
+                txtUsername.ForeColor = Color.White;
+            }
+        }
+
+        private void txtUsername_Leave(object sender, EventArgs e)
+        {
+            if (txtUsername.Text == "")
+            {
+                txtUsername.Text = "Username";
+                txtUsername.ForeColor = Color.Silver;
+            }
+
+        }
+
+        private void txtPassword_Enter(object sender, EventArgs e)
+        {
+            if (txtPassword.Text == "Password")
+            {
+                txtPassword.Text = "";
+                txtPassword.ForeColor = Color.White;
+                txtPassword.PasswordChar='*';
+            }
+        }
+
+        char? none = null;
+        private void txtPassword_Leave(object sender, EventArgs e)
+        {
+            if (txtPassword.Text == "")
+            {
+                txtPassword.Text = "Password";
+                txtPassword.ForeColor = Color.Silver;
+                txtPassword.PasswordChar = Convert.ToChar(none);
+            }
+        }
+
+        bool isthere;
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("SELECT * FROM Info", conn);
+            SqlDataReader rdr = cmd.ExecuteReader();
+
+            while (rdr.Read())
+            {
+                if (username == rdr["username"].ToString().TrimEnd() && password== rdr["pass"].ToString().TrimEnd())
+                {
+                    isthere = true;
+                    break;
+                }
+                else
+                {
+                    isthere = false;
+                }
+            }
+            conn.Close();
+
+            if (isthere)
+            {
+                MessageBox.Show("Giriş Başarılı");
+               
+            }
+            else
+            {
+                MessageBox.Show("Kullanıcı Adı veya Şifre Hatalı");
+            }
+
         }
     }
 }
