88421b91c4634b82b5e0b4e98ab39e8090650132
Arda24 <92623903+Ardaa24@users.noreply.github.com>
2025-10-04 19:12:18 +0300

    Update Signup.cs

--- Signup.cs
+++ Signup.cs
@@ -1,9 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +16,106 @@ namespace Giris_Panel
         public Signup()
         {
             InitializeComponent();
+        }
+
+        private void txtUsername_Enter(object sender, EventArgs e)
+        {
+            if (txtUsername.Text == "Username")
+            {
+                txtUsername.Text = "";
+                txtUsername.ForeColor = Color.White;
+            }
+        }
+
+        private void txtUsername_Leave(object sender, EventArgs e)
+        {
+            if (txtUsername.Text == "")
+            {
+                txtUsername.Text = "Username";
+                txtUsername.ForeColor = Color.Silver;
+            }
+        }
+
+        private void txtPassword_Enter(object sender, EventArgs e)
+        {
+            if (txtPassword.Text == "Password")
+            {
+                txtPassword.Text = "";
+                txtPassword.ForeColor = Color.White;
+                txtPassword.PasswordChar = '*';
+            }
+        }
+
+        
+        private void txtPassword_Leave(object sender, EventArgs e)
+        {
+            char? none = null;
+            if (txtPassword.Text == "")
+            {
+                txtPassword.Text = "Password";
+                txtPassword.ForeColor = Color.Silver;
+                txtPassword.PasswordChar = Convert.ToChar(none);
+            }
+        }
+
+        private void txtRePassword_Enter(object sender, EventArgs e)
+        {
+            if (txtPassword.Text == "RePassword")
+            {
+                txtPassword.Text = "";
+                txtPassword.ForeColor = Color.White;
+                txtPassword.PasswordChar = '*';
+            }
+        }
+
+        private void txtRePassword_Leave(object sender, EventArgs e)
+        {
+            char? none = null;
+            if (txtPassword.Text == "")
+            {
+                txtRePassword.Text = "RePassword";
+                txtRePassword.ForeColor = Color.Silver;
+                txtRePassword.PasswordChar = Convert.ToChar(none);
+            }
+        }
+
+        private void txtEmail_Enter(object sender, EventArgs e)
+        {
+
+            if (txtEmail.Text == "Emnail")
+            {
+                txtEmail.Text = "";
+                txtEmail.ForeColor = Color.White;
+            }
+
+        }
+
+        private void txtEmail_Leave(object sender, EventArgs e)
+        {
+            if (txtEmail.Text == "")
+            {
+                txtEmail.Text = "Email";
+                txtEmail.ForeColor = Color.Silver;
+            }
+        }
+
+        private void txtPhone_Enter(object sender, EventArgs e)
+        {
+
+            if (txtPhone.Text == "Phone")
+            {
+                txtPhone.Text = "";
+                txtPhone.ForeColor = Color.White;
+            }
+        }
+
+        private void txtPhone_Leave(object sender, EventArgs e)
+        {
+            if (txtPhone.Text == "")
+            {
+                txtPhone.Text = "Phone";
+                txtPhone.ForeColor = Color.Silver;
+            }
         }
     }
 }
