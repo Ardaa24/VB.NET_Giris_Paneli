76cf6759d101028e9eb88ae566579d4c7d0cc25b
Arda24 <92623903+Ardaa24@users.noreply.github.com>
2025-10-04 18:15:11 +0300

    Add files via upload

---
+++ Cryptology.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giris_Panel
+{
+    internal class Cryptology
+    {
+       public  static string Encryption(string text, int key)
+        {
+            char[] cry = text.ToCharArray();
+            string encryptedText = "";
+            foreach (char item in cry)
+            {
+                encryptedText+= Convert.ToChar(item + key).ToString();
+            }
+            return encryptedText;
+        }
+
+        public static string Decryption(string text, int key)
+        {
+            char[] cry = text.ToCharArray();
+            string decryptedText = "";
+            foreach (char item in cry)
+            {
+                decryptedText += Convert.ToChar(item - key).ToString();
+            }
+            return decryptedText;
+        }
+
+    }
+}
